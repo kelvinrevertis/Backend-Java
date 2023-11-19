7af34d3643b848e3c7224e1c2251e340b51fec87
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-11-19 18:13:40 -0300

    Move game on list

--- src/main/java/com/intensivaokelvin/dslist/services/GameListService.java
+++ src/main/java/com/intensivaokelvin/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.intensivaokelvin.dslist.services;
 
 import com.intensivaokelvin.dslist.dto.GameListDTO;
 import com.intensivaokelvin.dslist.entities.GameList;
+import com.intensivaokelvin.dslist.projections.GameMinProjection;
 import com.intensivaokelvin.dslist.repositories.GameListRepository;
+import com.intensivaokelvin.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex ){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for(int i = min; i<= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
